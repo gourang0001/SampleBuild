37361b03a986e31dd36a60d223505ae5d381a665
Gourang Chauhan <gourang.chauhan@magicsw.com>
2026-01-10 22:41:59 +0530

    api for error

--- controllers/buildagainController.js
+++ controllers/buildagainController.js
@@ -6,9 +6,8 @@ const buildagain = (req, res) => {
 };
 
 const devcheck = (req, res) => {
-    const { message } = req.body;
     res.status(200).json({
-        receivedMessage: message || 'No message provided',
+        receivedData: req.body,
         status: 'success'
     });
 };
